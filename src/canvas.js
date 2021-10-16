0e8e489c4521d71beed75c04a42ec9bba559fa0b
Akshay Nair <phenax5@gmail.com>
2021-10-16 20:45:24 +0530

    refactors stuff to save my reputation

---
+++ src/canvas.js
@@ -0,0 +1,34 @@
+import { clamp } from 'ramda'
+
+export const createCanvas = (size) => {
+  const $canvas = document.createElement("canvas");
+  $canvas.width = size.w;
+  $canvas.height = size.h;
+  $canvas.style.border = "1px solid pink";
+
+  const ctx = $canvas.getContext("2d");
+
+  return { $canvas, ctx };
+};
+
+export const forEachPixel = (ctx, size, fn) => {
+  const imageData = ctx.getImageData(0, 0, size.w, size.w);
+
+  for (
+    let i = 0, pixelCount = 0;
+    i < imageData.data.length;
+    i += 4, pixelCount++
+  ) {
+    const x = (pixelCount % size.h) - size.w / 2;
+    const y = Math.floor(pixelCount / size.h) - size.h / 2;
+    const [r, g, b, a] = fn(x, y) || [0, 0, 0, 0];
+
+    imageData.data[i] = r;
+    imageData.data[i + 1] = g;
+    imageData.data[i + 2] = b;
+    imageData.data[i + 3] = 255 * clamp(0, 1, a);
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+};
+
--- src/main.js
+++ src/main.js
@@ -1,94 +1,43 @@
-import {clamp, gt, T} from "ramda";
-import * as R from 'ramda'
+import * as R from "ramda";
+import * as C from "./complex";
+import * as functions from "./functions";
+import { createCanvas, forEachPixel } from './canvas'
 import "../style.css";
 
-const DIMENSIONS = { w: 900, h: 900 };
 const MAX_ITERATIONS = 100;
-const SCALE = 300;
 
-const Complex = (re, im) => ({ re, im });
-Complex.add = (a, b) => Complex(a.re + b.re, a.im + b.im);
-Complex.minus = (a, b) => Complex(a.re - b.re, a.im - b.im);
-Complex.scalarMul = (n, c) => Complex(c.re * n, c.im * n);
-Complex.mul = (a, b) =>
-  Complex(a.re * b.re - a.im * b.im, a.re * b.im + a.im * b.re);
-Complex.div = (a, b) => {
-  const mul = 1 / (b.re ** 2 + b.im ** 2);
-  const c = Complex.mul(a, Complex(b.re, -b.im));
-  return Complex.scalarMul(mul, c);
-};
-Complex.magnitude = (c) => Math.sqrt(c.re ** 2 + c.im ** 2);
-Complex.distance = (a, b) => Complex.magnitude(Complex.minus(a, b));
-Complex.pow = (c, n) => (n <= 1 ? c : Complex.mul(c, Complex.pow(c, n - 1)));
+const getAlpha = n => (n * 10) / MAX_ITERATIONS;
+const getColor = R.cond([
+  [R.gt(R.__, 13), n => [70, 130, 255, getAlpha(n)]],
+  [R.gt(R.__, 1), n => [70, 255, 130, getAlpha(n)]],
+  [R.T, n => [30, 30, 30, getAlpha(n)]],
+]);
 
-const newtonsMethod = (z, { fn, dfn }) => {
-  // Z' = Z - a*P(Z)/P'(Z)
-  const a = Complex(1, 0)
-  const step = Complex.div(fn(z), dfn(z));
-  return Complex.minus(z, Complex.mul(a, step));
-};
-
-const getRootColor = (c, options) => {
-  let z = c;
-  let nextZ = newtonsMethod(z, options);
-
-  let n;
-  for (n = 0; n < MAX_ITERATIONS; n++) {
-    nextZ = newtonsMethod(nextZ, options);
-
-    if (Complex.distance(nextZ, z) < 0.001) break;
-
-    z = nextZ;
-  }
-
-  const alpha = n*10/MAX_ITERATIONS
-
-  const getColor = R.cond([
-    [gt(R.__, 13), () => [70, 130, 255, alpha]],
-    [gt(R.__, 1), () => [70, 255, 130, alpha]],
-    [T, () => [30, 30, 30, alpha]],
-  ])
-
-  return getColor(n)
-};
-
-const init = () => {
-  const $canvas = document.createElement("canvas");
+const init = ({ func, size, scale = 200 }) => {
+  const { $canvas, ctx } = createCanvas(size);
   document.body.appendChild($canvas);
-  $canvas.width = DIMENSIONS.w;
-  $canvas.height = DIMENSIONS.h;
-  $canvas.style.border = "1px solid pink";
 
-  const ctx = $canvas.getContext("2d");
+  const { fn, dfn, a } = func
 
-  const fn = (c) => Complex.minus(Complex.pow(c, 3), Complex(1, 0));
-  const dfn = (c) => Complex.scalarMul(3, Complex.pow(c, 2));
+  forEachPixel(ctx, size, (x, y) => {
+    let z = C.scalarMul(1 / scale, C.make(x, y));
 
-  try {
-    const imageData = ctx.getImageData(0, 0, DIMENSIONS.w, DIMENSIONS.w);
+    let nextZ, n;
+    for (n = 0; n < MAX_ITERATIONS; n++) {
+      // Z' = Z - a*P(Z)/P'(Z)
+      const step = C.div(fn(z), dfn(z));
+      nextZ = C.minus(z, C.mul(a, step));
 
-    for (
-      let i = 0, pixelCount = 0;
-      i < imageData.data.length;
-      i += 4, pixelCount++
-    ) {
-      const col = (pixelCount % DIMENSIONS.h) - DIMENSIONS.w / 2;
-      const row = Math.floor(pixelCount / DIMENSIONS.h) - DIMENSIONS.h / 2;
-
-      const c = Complex.scalarMul(1 / SCALE, Complex(col, row));
-
-      const [r, g, b, a] = getRootColor(c, { fn, dfn });
-
-      imageData.data[i] = r;
-      imageData.data[i + 1] = g;
-      imageData.data[i + 2] = b;
-      imageData.data[i + 3] = 255*clamp(0, 1, a);
+      if (C.distance(nextZ, z) < 0.001) break;
+      z = nextZ;
     }
 
-    ctx.putImageData(imageData, 0, 0);
-  } catch (e) {
-    console.error(e);
-  }
+    return getColor(n);
+  });
 };
 
-init();
+init({
+  func: functions.x3_1_1,
+  size: { w: 900, h: 900 },
+  scale: 200,
+})
