2d666838548e74a5f24793764898d339c057ae08
Akshay Nair <phenax5@gmail.com>
2021-10-16 21:54:12 +0530

    trying animations and shit

--- src/functions.js
+++ src/functions.js
@@ -2,31 +2,30 @@ import * as C from './complex'
 
 const one = C.make(1, 0)
 
-export const sin = { fn: C.sin, dfn: C.cos, a: one };
+export const sin = { fn: C.sin, dfn: C.cos };
 
-export const cos = { fn: C.cos, dfn: C.sin, a: one };
+export const cos = { fn: C.cos, dfn: C.sin, zero: 1 };
 
 export const x3_1_1 = {
-  fn: z => C.minus(C.pow(z, 3), C.make(1, 0)),
-  dfn: z => C.scalarMul(3, C.pow(z, 2)),
+  //fn: z => C.minus(C.pow(z, 3), C.make(1, 0)),
+  fn: z => C.minus(C.mul(z, C.mul(z, z)), C.make(1, 0)),
+  dfn: z => C.scalarMul(3, C.mul(z, z)),
   a: C.make(1, 0)
 }
 
-export const x1_1_2 = {
-  fn: z => C.minus(C.pow(z, 3), C.make(1, 0)),
-  dfn: z => C.scalarMul(3, C.pow(z, 2)),
-  a: C.make(2, 0)
+export const x3_1_2 = {
+  ...x3_1_1,
+  a: C.make(2, 0),
+  zero: 0.5,
 }
 
 export const x5_1 = {
   fn: z => C.minus(C.pow(z, 5), one),
   dfn: z => C.scalarMul(5, C.pow(z, 4)),
-  a: one,
 }
 
 export const x5_x3_1 = {
   fn: z => C.minus(C.add(C.pow(z, 5), C.pow(z, 3)), one),
   dfn: z => C.add(C.scalarMul(5, C.pow(z, 4)), C.scalarMul(3, C.pow(z, 2))),
-  a: one,
 }
 
--- src/main.js
+++ src/main.js
@@ -13,14 +13,11 @@ const getColor = R.cond([
   [R.T, n => [30, 30, 30, getAlpha(n)]],
 ]);
 
-const init = ({ func, size, scale = 200 }) => {
-  const { $canvas, ctx } = createCanvas(size);
-  document.body.appendChild($canvas);
-
-  const { fn, dfn, a } = func
+const draw = ({ ctx, func, size, scale = 200 }) => {
+  const { fn, dfn, a = C.make(1, 0), zero = 0.0001 } = func
 
   forEachPixel(ctx, size, (x, y) => {
-    let z = C.scalarMul(1 / scale, C.make(x, y));
+    let z = C.make(x/scale, y/scale);
 
     let nextZ, n;
     for (n = 0; n < MAX_ITERATIONS; n++) {
@@ -28,7 +25,8 @@ const init = ({ func, size, scale = 200 }) => {
       const step = C.div(fn(z), dfn(z));
       nextZ = C.minus(z, C.mul(a, step));
 
-      if (C.distance(nextZ, z) < 0.001) break;
+      if (Math.abs(nextZ.re - z.re) < zero) break;
+      // if (C.distance(nextZ, z) < zero) break;
       z = nextZ;
     }
 
@@ -36,8 +34,32 @@ const init = ({ func, size, scale = 200 }) => {
   });
 };
 
-init({
-  func: functions.x3_1_1,
-  size: { w: 900, h: 900 },
-  scale: 200,
-})
+const init = (max, n, options) => {
+  const { ctx, size } = options
+  const m = n / max;
+
+  ctx.clearRect(0, 0, size.w, size.h)
+
+  draw({
+    ctx,
+    //func: functions.x3_1_1,
+    func: { ...functions.x3_1_1, a: C.make(m, 0) },
+    size,
+    scale: 100,
+  })
+
+  if (n < max) {
+    requestAnimationFrame(() => init(max, n + 1, options))
+  } else {
+    console.timeEnd('init')
+  }
+};
+
+const size = { w: 900, h: 900 }
+const { $canvas, ctx } = createCanvas(size);
+document.body.appendChild($canvas);
+
+console.time('init')
+init(50, 40, { ctx, size })
+// 32005.693115234375 ms
+
